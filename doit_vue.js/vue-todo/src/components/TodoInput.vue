d7bde304e3fd0b325926df452925d2b4f8e2a13e
hyunto <jhyunto@gmail.com>
2018-07-14 00:43:11 +0900

    Do it! Vue.js 입문 6장 완료

--- doit_vue.js/vue-todo/src/components/TodoInput.vue
+++ doit_vue.js/vue-todo/src/components/TodoInput.vue
@@ -4,14 +4,25 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fa fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header"> 경고</h3>
+      <span slot="footer" @click="showModal = false">
+         할 일을 입력하세요
+         <i class="closeModalBtn fa fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+  import Modal from './common/Modal.vue'
+
   export default {
     data() {
       return {
-        newTodoItem: ''
+        newTodoItem: '',
+        showModal: false
       }
     },
     methods: {
@@ -21,11 +32,16 @@
           // localStorage.setItem(value, value);
           this.$emit('addTodo', value);
           this.clearInput();
+        } else {
+          this.showModal = !this.showModal;
         }
       },
       clearInput() {
         this.newTodoItem = '';
       }
+    },
+    components: {
+      'Modal': Modal
     }
   }
 </script>
@@ -58,5 +74,14 @@
   .addBtn {
     color: white;
     vertical-align: middle;
+  }
+
+  .modal-header h3 {
+    margin-top: 0;
+    color: #42a9b9;
+  }
+
+  .closeModalBtn {
+    color: #42a9b9;
   }
 </style>
