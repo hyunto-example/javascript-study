c9a773144bfceb0e10f9eddc2eeb04fa9660ccc2
hyunto <jhyunto@gmail.com>
2018-07-13 09:10:11 +0900

    Do it! Vue.js 입문 05-1장 퀴즈 완료

--- doit_vue.js/05-1/quiz/app.js
+++ doit_vue.js/05-1/quiz/app.js
@@ -4,13 +4,14 @@ var app = new Vue({
     message: 'Hello Vue.js',
     // 할일 #1
     // 새로운 데이터 속성을 1개 추가하고, {{ }} 데이터 바인딩을 이용해 화면에 표시해 보세요.
+    banner: 'Welcome!!!',
 
-    uid: '10',
+    uid: '19',
     // 할일 #2
     // uid를 변경하고 해당 uid의 변경 여부를 크롬 개발자 도구의 '화면 요소 검사' 기능으로 확인해보세요.
     // p 태그의 id 값을 검사
 
-    flag: true
+    flag: false
     // 할일 #4
     // 위 flag 값을 false로 변경했을 때 화면에 어떤 영향을 주는지 확인해 보세요.
   },
@@ -26,6 +27,9 @@ var app = new Vue({
 
     // 할일 #3
     // eventMethod를 하나 추가하고 index.html의 #3 영역에 해당 이벤트를 실행하는 버튼을 추가하세요.
-    
+    clickEvt() {
+      alert('clicked');
+    }
+
   }
-})
+});
