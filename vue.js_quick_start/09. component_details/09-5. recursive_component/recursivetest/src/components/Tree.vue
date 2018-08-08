bec633fb826688f6196066ee172adc8ed2f06edb
hyunto <jhyunto@gmail.com>
2018-08-08 10:11:52 +0900

    Vue.js 퀵 스타트 09장 완료

---
+++ vue.js_quick_start/09. component_details/09-5. recursive_component/recursivetest/src/components/Tree.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li v-for="s in subs" v-bind:class="s.type" v-bind:key="s.name">
+            {{ s.name }}
+            <tree :subs="s.subs"></tree>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'tree',
+    props: [
+        'subs'
+    ]
+}
+</script>
+
+<style scoped>
+
+</style>
