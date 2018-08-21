1af2d33be479355ae05d0186c3ff34ef7bb06814
hyunto <jhyunto@gmail.com>
2018-08-21 13:31:21 +0900

    Vue.js 퀵 스타트  11장 Getter 완료

---
+++ vue.js_quick_start/11. vuex/countryapp/src/components/RegionButtons.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button class="region" v-for="region in regions" :key="region" v-bind:class="isSelected(region)" @click="changeRegion({region: region})">
+            {{ region }}
+        </button>
+    </div>
+</template>
+
+<script>
+import Constant from '../Constant';
+import {mapMutations } from 'vuex';
+
+export default {
+    name: "RegionButtons",
+    computed: {
+        regions() {
+            return this.$store.getters.regions;
+        },
+        currentRegion() {
+            return this.$store.getters.currentRegion;
+        }
+    },
+    methods: {
+        isSelected(region) {
+            if (region == this.currentRegion) {
+                return {
+                    selected: true
+                };
+            } else {
+                return {
+                    selected: false
+                }
+            }
+        },
+        ...mapMutations([
+            Constant.CHANGE_REGION
+        ])
+    }    
+}
+</script>
