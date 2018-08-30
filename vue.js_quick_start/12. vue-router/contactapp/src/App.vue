3463e1e8e72392e1ea3452793a0414a8697a3c8d
hyunto <jhyunto@gmail.com>
2018-08-30 13:49:58 +0900

    Vue.js 퀵 스타트 12장 진행
    - contactapp 버그 수정
    - 12.9.1 초기 설정
    - 12.9.2 vuex 코드 수정

---
+++ vue.js_quick_start/12. vue-router/contactapp/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="container">
+    <div class="page-header">
+      <h1 class="text-center">연락처 관리 애플리케이션</h1>
+      <p>(Dynamic Component + EventBus + Axios)</p>
+    </div>
+
+    <!-- AddContact, UpdateContact, UpdatePhoto 컴포넌트는 동적 컴포넌트 방식으로 currentView 옵션을 통해 나타낸다 -->
+    <component :is="currentView"></component>
+
+    <contactList></contactList>
+
+  </div>
+</template>
+
+<script>
+// import Vue from 'vue';
+import ContactList from './components/ContactList';
+import ContactForm from './components/ContactForm';
+import UpdatePhoto from './components/UpdatePhoto';
+
+import { mapState } from 'vuex';
+
+export default {
+  name: 'app',
+  components: {
+    ContactList,
+    ContactForm,
+    UpdatePhoto
+  },
+  computed: mapState([ 'currentView' ])
+}
+
+</script>
+
+<style scoped>
+#container {
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.js_quick_start/12. vue-router/contactapp/src/store/actions.js
@@ -0,0 +1,116 @@
+import Constant from '../Constant';
+import axios from 'axios';
+import CONF from '../Config';
+
+export default {
+    // ** vue-router 적용에 따라 삭제 **
+    // [Constant.ADD_CONTACT_FORM]: (store) => {
+    //     store.commit(Constant.ADD_CONTACT_FORM);
+    // },
+    // [Constant.EDIT_CONTACT_FORM]: (store, payload) => {
+    //     axios.get(CONF.FETCH_ONE.replace("${no}", payload.no))
+    //         .then((response) => {
+    //             store.commit(Constant.EDIT_CONTACT_FORM, { contact: response.data })
+    //         });
+    // },
+    // [Constant.EDIT_PHOTO_FORM]: (store, payload) => {
+    //     axios.get(CONF.FETCH_ONE.replace("${no}", payload.no))
+    //         .then((response) => {
+    //             store.commit(Constant.EDIT_PHOTO_FORM, { contact: response.data });
+    //         });
+    // },
+    // [Constant.CANCEL_FORM]: (store) => {
+    //     store.commit(Constant.CANCEL_FORM);
+    // },
+
+    [Constant.FETCH_CONTACTS]: (store, payload) => {
+        var pageno;
+        if (typeof payload === "undefined" || typeof payload.pageno === "undefined")
+            pageno = 1;
+        else
+            pageno = payload.pageno;
+
+        var pagesize = store.state.contactlist.pagesize;
+
+        axios.get(CONF.FETCH, {
+            params: {
+                pageno: pageno,
+                pagesize: pagesize
+            }
+        }).then((response) => {
+            store.commit(Constant.FETCH_CONTACTS, {
+                contactlist: response.data
+            });
+        });
+    },
+
+    [Constant.ADD_CONTACT]: (store) => {
+        axios.post(CONF.ADD, store.state.contact)
+            .then((response) => {
+                if (response.data.status === "success") {
+                    // store.dispatch(Constant.CANCEL_FORM);
+                    store.dispatch(Constant.FETCH_CONTACTS, { pageno: 1 });
+                } else {
+                    console.log('연락처 추가 실패 : ' + response.data.message);
+                }
+            })
+            .catch((ex) => {
+                console.log('addContact failed', ex);
+            });
+    },
+
+    [Constant.UPDATE_CONTACT]: (store) => {
+        var currentPageNo = store.state.contactlist.pageno;
+        var contact = store.state.contact;
+
+        axios.put(CONF.UPDATE.replace("${no}", contact.no), contact)
+            .then((response) => {
+                if (response.data.status === "success") {
+                    // store.dispatch(Constant.CANCEL_FORM);
+                    store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+                } else {
+                    console.log('연락처 변경 실패 : ' + response.data.message);
+                }
+            })
+            .catch((ex) => {
+                console.log('updateContact failed', ex);
+            });
+    },
+
+    [Constant.UPDATE_PHOTO]: (store, payload) => {
+        var currentPageNo = store.state.contactlist.pageno;
+        var data = new FormData();
+        data.append('photo', payload.file);
+
+        axios.post(CONF.UPDATE_PHOTO.replace("${no}", payload.no), data)
+            .then((response) => {
+                // store.dispatch(Constant.CANCEL_FORM);
+                store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo });
+            });
+    },
+
+    [Constant.DELETE_CONTACT]: (store, payload) => {
+        var currentPageNo = store.state.contactlist.pageno;
+
+        axios.delete(CONF.DELETE.replace("${no}", payload.no))
+            .then((response) => {
+                if (response.data.status === "success")
+                    store.dispatch(Constant.FETCH_CONTACTS, { pageno: currentPageNo })
+                else
+                    console.log('연락처 삭제 실패 : ' + response.data.message);
+            }).catch((ex) => {
+                console.log('delete failed', ex);
+            });
+    },
+
+    [Constant.FETCH_CONTACT_ONE]: (store, payload) => {
+        axios.get(CONF.FETCH_ONE.replace("${no}", payload.no))
+            .then((response) => {
+                store.commit(Constant.FETCH_CONTACT_ONE, { contact: response.data });
+            })
+    },
+
+    [Constant.INITIALIZE_CONTACT_ONE]: (store) => {
+        store.commit(Constant.INITIALIZE_CONTACT_ONE);
+    }
+}
